0795692a03d5b98f6e3beb93c20bf8aed9d42e24
RoffyWoo <69416808+BostonRunner@users.noreply.github.com>
2025-04-24 00:48:33 +0800

    Update blktrace.sh

--- blktrace.sh
+++ blktrace.sh
@@ -1,2 +1,3 @@
-cd ./results
-blktrace -d /dev/vdb -w 2700 -o - | blkparse -i - >> result.log &
+#!/bin/bash
+cd "${RESULT_DIR:-./results}"
+blktrace -d /dev/vdb -w 2700 -o - | blkparse -i - >> result.log
