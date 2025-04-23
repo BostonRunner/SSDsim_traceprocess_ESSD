e3e10e67ef99542521615b54e7fca8514a379c3a
RoffyWoo <69416808+BostonRunner@users.noreply.github.com>
2025-04-24 00:47:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-rm -rf results/
-mkdir results
-cd ./results || exit
+USE_CONTAINERS=${USE_CONTAINERS:-6}
+RESULT_DIR="result${USE_CONTAINERS}"
+
+rm -rf "$RESULT_DIR/"
+mkdir "$RESULT_DIR"
+cd "$RESULT_DIR" || exit
 echo "directory created"
 touch result.txt
 touch result.log
 echo "result.txt result.log created"
 echo "blktrace activating"
 cd ..
+
 docker kill $(docker ps -q)
 docker system prune -af --volumes
 sleep 3
-./blktrace.sh
+
+RESULT_DIR="$RESULT_DIR" ./blktrace.sh
 sleep 3
-./IO.sh
+USE_CONTAINERS=$USE_CONTAINERS RESULT_DIR="$RESULT_DIR" ./IO.sh
 sleep 3
 
-
-cd ./results || exit
+cd "$RESULT_DIR" || exit
 # grep 'A' result.log | grep " W " > result_temp.txt
 grep 'W' result.log > result_temp.txt
 sort -n -k 4 result_temp.txt > result.txt
 cd ..
 
 echo "processing I/O trace..." & sleep 60
-echo 3 | sudo tee /proc/sys/vm/drop_caches   # 
+echo 3 | sudo tee /proc/sys/vm/drop_caches   #
 # sudo find /mnt/emu -xdev -type f > /dev/null  #
-python3 main.py
-python3 trans.py
+python3 main.py "$RESULT_DIR"
+python3 trans.py "$RESULT_DIR"
 echo "program finished"
