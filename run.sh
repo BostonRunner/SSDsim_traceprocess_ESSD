c43f11dcc6ab97f4ef6eba8e6b6076f74ced4e89
RoffyWoo <69416808+BostonRunner@users.noreply.github.com>
2025-08-12 20:53:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,20 +16,20 @@ docker kill $(docker ps -q)
 docker system prune -af --volumes
 sleep 3
 
-RESULT_DIR="$RESULT_DIR" ./blktrace.sh
-sleep 3
+# RESULT_DIR="$RESULT_DIR" ./blktrace.sh
+# sleep 3
 USE_CONTAINERS=$USE_CONTAINERS RESULT_DIR="$RESULT_DIR" ./IO.sh
 sleep 3
 
-cd "$RESULT_DIR" || exit
+# cd "$RESULT_DIR" || exit
 # grep 'A' result.log | grep " W " > result_temp.txt
-grep -E " W | WS " result.log | grep " D " > result_temp.txt
-sort -n -k 4 result_temp.txt > result.txt
-cd ..
+# grep -E " W | WS " result.log | grep " D " > result_temp.txt
+# sort -n -k 4 result_temp.txt > result.txt
+# cd ..
 
-echo "processing I/O trace..." & sleep 60
-echo 3 | sudo tee /proc/sys/vm/drop_caches   #
+# echo "processing I/O trace..." & sleep 60
+# echo 3 | sudo tee /proc/sys/vm/drop_caches   #
 # sudo find /mnt/emu -xdev -type f > /dev/null  #
-python3 main.py "$RESULT_DIR"
-python3 trans.py "$RESULT_DIR"
+# python3 main.py "$RESULT_DIR"
+# python3 trans.py "$RESULT_DIR"
 echo "program finished"
